641884a719b41af63ddb5f0a2b8e8a136bb232b0
GOIO Enrique <egoiomab@gmail.com>
2020-08-27 21:49:47 +0200

    Make pin entity timestampable, Listing on index by createdAt desc

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -18,7 +18,7 @@ class PinsController extends AbstractController
     {
 //       dd($pinRepository->findAll());
 
-        $pins = $pinRepository->findAll();
+        $pins = $pinRepository->findBy([], ['createdAt' => 'DESC']);
          return $this->render('pins/index.html.twig', compact('pins'));
     }
 
